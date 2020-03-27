545ae0cf109bbade0766f11a8821690480ddd7cc
Joao Rebelo <joaorebelo.ar@gmail.com>
2020-03-27 08:05:30 +0000

    Updating to non deprecated Assert classes

--- src/test/java/com/jrebelo/jenkins/demo/DemoImplTest.java
+++ src/test/java/com/jrebelo/jenkins/demo/DemoImplTest.java
@@ -4,18 +4,18 @@ import java.time.Duration;
 
 import org.junit.Test;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 import reactor.core.publisher.DirectProcessor;
 
 public class DemoImplTest {
 
     @Test
     public void testDoesntExceedLimit(){
-        DirectProcessor<Object> testProcessor = DirectProcessor.create();
+        final DirectProcessor<Object> testProcessor = DirectProcessor.create();
         final long numberOfExpectedElements = 10;
-        DemoInterface demo = new DemoImpl(testProcessor, numberOfExpectedElements, Duration.ZERO);
-        
-        var stream = demo.stream();
+        final DemoInterface demo = new DemoImpl(testProcessor, numberOfExpectedElements, Duration.ZERO);
+
+        final var stream = demo.stream();
         testProcessor.sink().next(new Object());
         
         final long numberOfElements = stream.count();
